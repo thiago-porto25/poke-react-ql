20ced5d2305ae126d7897d770fc5df0504d029e4
thiago-porto25 <thiago_porto25@hotmail.com>
2021-07-21 19:51:11 -0300

    set up apollo client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { PokemonsContainer } from './containers/PokemonsContainer'
 
 function App() {
-  return <div className="App">Test</div>
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/',
+  })
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  )
 }
 
 export default App
---
+++ src/containers/PokemonsContainer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function PokemonsContainer() {
+  return <p>i'm pokemon container</p>
+}
