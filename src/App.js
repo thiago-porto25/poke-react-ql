914201a474a3f2adcc70ab848d47d82b94f7506f
thiago-porto25 <thiago_porto25@hotmail.com>
2021-07-22 12:23:52 -0300

    add a header, style it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { PokemonsContainer } from './containers/PokemonsContainer'
+import { Header } from './components/Header'
 
 function App() {
   const client = new ApolloClient({
@@ -10,6 +11,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <main>
+        <Header />
         <PokemonsContainer />
       </main>
     </ApolloProvider>
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <header className="headerContainer">
+      <h1>Poke Search</h1>
+    </header>
+  )
+}
